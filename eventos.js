cb65f2e29317a924f7b153117130a280d0c25e40
vmazzulla <vmazzulla12@gmail.com>
2024-08-17 18:33:12 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -1,5 +1,10 @@
 const div = document.getElementById("div");
+const boton = document.getElementsByTagName("button");
 
 div.addEventListener('click', () =>{
     alert('Hola! soy el div');
+});
+
+boton[0].addEventListener('click', (event) =>{
+    event.stopPropagation();
 });
